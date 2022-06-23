876e264f7655b4a2dd25eb1cb42b7b3d7a43dd91
Oleg Ilnytskyy <ilnytskyy67922@gmail.com>
2022-06-23 13:13:15 +0300

    +LAyout

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', \App\Http\Controllers\AdminController::class);
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
