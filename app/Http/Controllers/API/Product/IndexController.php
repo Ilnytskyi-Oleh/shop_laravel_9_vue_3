c4b49639ad85dfe0de482cc61bad06944b3e3ab4
Valdogar <67922@ukr.net>
2022-07-23 23:27:11 +0300

    18

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\IndexProductResource;
 use App\Models\Product;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
+
         $products = Product::all();
-        return ProductResource::collection($products);
+        return IndexProductResource::collection($products);
     }
 }
