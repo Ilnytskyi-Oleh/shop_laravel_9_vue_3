934178d7bd3d0a90fb8de2bd5c8ecf2c06643c8f
Valdogar <67922@ukr.net>
2022-07-27 21:58:50 +0300

    Filterable

---
+++ app/Http/Filters/ProductFilter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class ProductFilter extends AbstractFilter
+{
+
+    const CATEGORIES = 'categories';
+    const COLORS = 'colors';
+    const TAGS = 'tags';
+    const PRICE = 'price';
+
+    protected function getCallbacks(): array
+    {
+        return [
+            self::CATEGORIES => [$this, 'categories'],
+            self::COLORS => [$this, 'colors'],
+            self::TAGS => [$this, 'tags'],
+            self::PRICE => [$this, 'price'],
+        ];
+    }
+
+    public function categories(Builder $builder, $value)
+    {
+        $builder->whereIn('category_id', $value);
+    }
+
+    public function colors(Builder $builder, $value)
+    {
+        $builder->whereIn('color_id', $value);
+    }
+
+    public function tags(Builder $builder, $value)
+    {
+        $builder->whereHas('tags', function ($b) use ($value){
+            $b->whereIn('tag_id', $value);
+        });
+    }
+
+    public function price(Builder $builder, $value)
+    {
+        $builder->whereBetween($value['from'], $value['to']);
+    }
+}
